5c838412ceb361b2b095f1a32c2e0ea0b3f3b4d6
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-03-13 10:23:07 +0100

    navibar-wstepna-wersja

--- apps/gnome-mobile/src/app/gallery-page.tsx
+++ apps/gnome-mobile/src/app/gallery-page.tsx
@@ -12,31 +12,28 @@ export default function SignInScreen() {
 
   return (
     <View className="flex-1 justify-end">
-      
-      
-
       <View>
         <View className="flex-row border rounded-full bg-gray-700 border-gray-700 mt-4 mb-4 w-full  mx-auto">
-          <Button className="bg-red-700 border rounded-full border-red-700 ml-4 mr-2 my-2"
-          onPress={() => replace("/sign-in")}>
+          <Button className="bg-gray-700 border rounded-full border-gray-700 ml-4 mr-2 my-2"
+          onPress={() => replace("/main-page")}>
             <Text className="text-white font-Afacad">
               Główna
             </Text>
           </Button>
-          <Button className="bg-red-700 border rounded-full border-red-700 mx-2 my-2"
-          onPress={() => replace("/sign-in")}>
+          <Button className="bg-gray-700 border rounded-full border-gray-700 mx-2 my-2"
+          onPress={() => replace("/collect-page")}>
             <Text className="text-white font-Afacad">
               Kolekcja
             </Text>
           </Button>
-          <Button className="bg-red-700 border rounded-full border-red-700 mx-2 my-2"
-          onPress={() => replace("/sign-in")}>
+          <Button className="bg-gray-700 border rounded-full border-gray-700 mx-2 my-2"
+          onPress={() => replace("/friend-page")}>
             <Text className="text-white font-Afacad">
               Znajomi
             </Text>
           </Button>
           <Button className="bg-red-700 border rounded-full border-red-700 ml-2 mr-4 my-2"
-          onPress={() => replace("/sign-in")}>
+          onPress={() => replace("/gallery-page")}>
             <Text className="text-white font-Afacad">
               Galeria
             </Text>
--- apps/gnome-mobile/src/app/login.tsx
+++ apps/gnome-mobile/src/app/login.tsx
@@ -40,7 +40,7 @@ export default function SignInScreen() {
           </Text>
           
         </View>
-        <Button onPress={() => replace("/navi")}
+        <Button onPress={() => replace("/main-page")}
         className="border rounded-3xl border-red-800 bg-red-800 mt-4 mb-4 w-80 h-20 mx-auto">
           <Text>Zaloguj</Text>
         </Button>
