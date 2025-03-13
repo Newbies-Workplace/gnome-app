013610d768886371f2165cde9efaee0eb5b92e68
N3s4a <nesakrad56@gmail.com>
2025-03-13 18:25:47 +0100

    feat: Navbar(not working) and images 1/2

--- apps/gnome-web/src/main.tsx
+++ apps/gnome-web/src/main.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AdminPage } from "./pages/AdminPage.tsx";
 import { GnomePage } from "./pages/GnomePage.tsx";
 import { HomePage } from "./pages/HomePage.tsx";
+import { LogPage } from "./pages/LogPage.tsx";
 import { ReportPage } from "./pages/ReportPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")!).render(
       <Route index element={<HomePage />} />
       <Route path={"report"} element={<ReportPage />} />
       <Route path={"gnomes/:gnomeId"} element={<GnomePage />} />
+      <Route path={"log"} element={<LogPage />} />
+      <Route path={"admin"} element={<AdminPage />} />
     </Routes>
   </BrowserRouter>,
 );
