ce725b61020867f47f6201b3e83de8faa9e93172
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-03-13 11:56:42 +0100

    fix: yarn lint

--- apps/gnome-mobile/src/app/friend-page.tsx
+++ apps/gnome-mobile/src/app/friend-page.tsx
@@ -14,29 +14,29 @@ export default function SignInScreen() {
     <View className="flex-1 justify-end">
       <View>
         <View className="flex-row border rounded-full bg-gray-700 border-gray-700 mt-4 mb-4 w-full  mx-auto">
-          <Button className="bg-gray-700 border rounded-full border-gray-700 ml-4 mr-2 my-2"
-          onPress={() => replace("/main-page")}>
-            <Text className="text-white font-Afacad">
-              Główna
-            </Text>
+          <Button
+            className="bg-gray-700 border rounded-full border-gray-700 ml-4 mr-2 my-2"
+            onPress={() => replace("/main-page")}
+          >
+            <Text className="text-white font-Afacad">Główna</Text>
           </Button>
-          <Button className="bg-gray-700 border rounded-full border-gray-700 mx-2 my-2"
-          onPress={() => replace("/collect-page")}>
-            <Text className="text-white font-Afacad">
-              Kolekcja
-            </Text>
+          <Button
+            className="bg-gray-700 border rounded-full border-gray-700 mx-2 my-2"
+            onPress={() => replace("/collect-page")}
+          >
+            <Text className="text-white font-Afacad">Kolekcja</Text>
           </Button>
-          <Button className="bg-red-700 border rounded-full border-red-700 mx-2 my-2"
-          onPress={() => replace("/friend-page")}>
-            <Text className="text-white font-Afacad">
-              Znajomi
-            </Text>
+          <Button
+            className="bg-red-700 border rounded-full border-red-700 mx-2 my-2"
+            onPress={() => replace("/friend-page")}
+          >
+            <Text className="text-white font-Afacad">Znajomi</Text>
           </Button>
-          <Button className="bg-gray-700 border rounded-full border-gray-700 ml-2 mr-4 my-2"
-          onPress={() => replace("/gallery-page")}>
-            <Text className="text-white font-Afacad">
-              Galeria
-            </Text>
+          <Button
+            className="bg-gray-700 border rounded-full border-gray-700 ml-2 mr-4 my-2"
+            onPress={() => replace("/gallery-page")}
+          >
+            <Text className="text-white font-Afacad">Galeria</Text>
           </Button>
         </View>
       </View>
