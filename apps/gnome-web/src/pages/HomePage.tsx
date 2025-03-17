f9ba003bb87079500c9494e218a5a173f5d26553
Bondarenko Anastasiia <tasyabondarenko840@gmail.com>
2025-03-17 09:19:47 +0100

    - bgelement

--- apps/gnome-web/src/pages/HomePage.tsx
+++ apps/gnome-web/src/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./Navbar";
-import BackgroundElement from "./bgelement";
 
 export const HomePage = () => {
   return (
     <div>
       <Navbar />
       <h1>Witaj na stronie głównej!</h1>
-      <BackgroundElement />
     </div>
   );
 };
