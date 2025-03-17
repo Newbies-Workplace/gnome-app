19e9ad8a9ce06e850ee87a11619623a7762454fd
Bondarenko Anastasiia <tasyabondarenko840@gmail.com>
2025-03-17 14:01:07 +0100

    feat: Add Pages

--- apps/gnome-web/src/pages/AdminPage.tsx
+++ apps/gnome-web/src/pages/AdminPage.tsx
@@ -1,10 +1,31 @@
 import Navbar from "./Navbar";
 
 export const AdminPage = () => {
+  const buttonStyle: React.CSSProperties = {
+    backgroundColor: "#1E201E",
+    color: "#757A75",
+    borderRadius: "50px",
+    padding: "60px 50px",
+    border: "none",
+    cursor: "pointer",
+    margin: "10px",
+  };
   return (
     <div>
       <Navbar />
       <h1>Admin</h1>
+      <button
+        style={buttonStyle}
+        onClick={() => (window.location.href = "/addgnome")}
+      >
+        Dodaj krasnala do bazy
+      </button>
+      <button
+        style={buttonStyle}
+        onClick={() => (window.location.href = "/lostinfo")}
+      >
+        Sprawdź ostatnie zgłoszenia
+      </button>
     </div>
   );
 };
---
+++ apps/gnome-web/src/pages/LostInfoPage.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export const LostInfoPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Od ostatniego logowania zgłoszono 3 krasnali</h1>
+    </div>
+  );
+};
