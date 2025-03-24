63bbb00e0ebdd9f39dac244fceaea80fa1fff16a
Bondarenko Anastasiia <tasyabondarenko840@gmail.com>
2025-03-24 09:01:37 +0100

    addinfopage

--- apps/gnome-web/src/components/LinkButton.tsx
+++ apps/gnome-web/src/components/LinkButton.tsx
@@ -3,13 +3,23 @@ import { Link } from "react-router-dom";
 type LinkButtonProps = {
   to: string;
   label: string;
+  py: string;
+  px: string;
 };
 
-export const LinkButton: React.FC<LinkButtonProps> = ({ to, label }) => {
+export const LinkButton: React.FC<LinkButtonProps> = ({
+  to,
+  label,
+  px,
+  py,
+}) => {
   return (
     <div>
       <Link to={to}>
-        <button className="bg-[#1E201E] text-[#757A75] rounded-full px-12 py-10 border-none cursor-pointer m-2">
+        <button
+          style={{ padding: `${py}px ${px}px` }}
+          className="bg-[#1E201E] text-[#757A75] rounded-full border-none cursor-pointer m-2 w-40 h-12 flex items-center justify-center inline-flex"
+        >
           {label}
         </button>
       </Link>
