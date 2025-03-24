ef4daa3a74129919507851ca490b6b1cca19dbdd
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-03-24 12:40:27 +0100

    fix

--- apps/gnome-mobile/src/app/(app)/(tabs)/index.tsx
+++ apps/gnome-mobile/src/app/(app)/(tabs)/index.tsx
@@ -4,13 +4,16 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Text } from "@/components/ui/text";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useNavigation } from "@react-navigation/native";
+import { useRouter } from "expo-router";
 import React from "react";
 import { Image, TouchableOpacity, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
-const MapScreen = () => {
-  const navigation = useNavigation();
+export default function MapScreen() {
   const { user } = useAuthStore();
+  const navigation = useNavigation();
+  const { replace } = useRouter();
+
   const region = {
     latitude: 51.1079, // współrzędne Wrocławia
     longitude: 17.0385,
@@ -20,7 +23,6 @@ const MapScreen = () => {
 
   return (
     <View className="flex-1 bg-white">
-      {/* Top Bar */}
       <View className="flex-row justify-between items-center p-4 bg-gray-800">
         <Avatar alt="Your avatar" className="w-20 h-20">
           <AvatarImage source={{ uri: user.pictureUrl }} />
@@ -29,16 +31,14 @@ const MapScreen = () => {
           </AvatarFallback>
         </Avatar>
         <View className="flex-row justify-between items-center w-20">
-          <TouchableOpacity onPress={() => navigation.navigate("Friends")}>
+          <TouchableOpacity onPress={() => navigation.navigate("friends")}>
             <FriendIcon width={24} height={24} fill="#fff" />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("Teams")}>
+          <TouchableOpacity onPress={() => navigation.navigate("teams")}>
             <TeamIcon width={24} height={24} fill="#fff" />
           </TouchableOpacity>
         </View>
       </View>
-
-      {/* Map View */}
       <View className="flex-1">
         <MapView
           className="flex-1"
@@ -66,10 +66,17 @@ const MapScreen = () => {
             description="Ciągle pchają ten nieszczęsny kamień"
             icon={require("@/assets/images/krasnal.png")}
           />
+          <Marker
+            coordinate={{
+              latitude: 51.10947379220885,
+              longitude: 17.057842134960516,
+            }}
+            title="Krasnal Mędruś"
+            description="Ponąć studenci przychodzą do niego po porady"
+            icon={require("@/assets/images/krasnal.png")}
+          />
         </MapView>
       </View>
     </View>
   );
-};
-
-export default MapScreen;
+}
