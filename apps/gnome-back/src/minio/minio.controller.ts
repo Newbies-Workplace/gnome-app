3bbc43d8ac5ac6f6b4f24f4a8750a3f8d0da97ad
mati <MeenQ12@gmail.com>
2025-03-24 13:12:37 +0100

    setup, controllers, service

---
+++ apps/gnome-back/src/minio/minio.controller.ts
@@ -0,0 +1,47 @@
+import type { JWTUser } from "@/auth/jwt/JWTUser";
+import { User } from "@/auth/jwt/jwtuser.decorator";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import type { MinioService } from "./minio.service";
+
+@Controller("minio")
+export class MinioController {
+  constructor(private readonly minioService: MinioService) {}
+
+  @Post("image")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @User() user: JWTUser,
+    @Body() body: { gnomeId: string },
+  ) {
+    await this.minioService.createBucketIfNotExists();
+    const fileName = await this.minioService.uploadFile(
+      file,
+      user.id,
+      body.gnomeId,
+    );
+    return fileName;
+  }
+
+  @Get("image/:fileName")
+  async getFileUrl(@Param("fileName") fileName: string) {
+    const fileUrl = await this.minioService.getFileUrl(fileName);
+    return fileUrl;
+  }
+
+  @Delete("image/:fileName")
+  async deleteFile(@Param("fileName") fileName: string) {
+    await this.minioService.deleteFile(fileName);
+    return fileName;
+  }
+}
