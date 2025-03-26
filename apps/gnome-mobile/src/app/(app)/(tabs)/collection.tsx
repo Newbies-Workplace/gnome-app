024c51edc275632adfb77c513bcb9e2dcb2bb8d0
Maciej Radaj <radajmaciej@gmail.com>
2025-03-26 13:00:03 +0100

    dodalem wyswietlanie gnomow z backendu na collection w gnomeCard komponencie - usunelem niepotrzebny komentarz z pliku useGnomeStore.ts

--- apps/gnome-mobile/src/app/(app)/(tabs)/collection.tsx
+++ apps/gnome-mobile/src/app/(app)/(tabs)/collection.tsx
@@ -1,33 +1,31 @@
-import { Button } from "@/components/ui/button";
+import { GnomeCard } from "@/components/ui/GnomeCard";
 import { useGnomeStore } from "@/store/useGnomeStore";
 import React, { useEffect } from "react";
-import { FlatList, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const GnomeList = () => {
   const { gnomes, fetchGnomes, loading, error } = useGnomeStore();
+
+  useEffect(() => {
+    fetchGnomes();
+  }, []);
 
   if (loading) return <Text>Ładowanie...</Text>;
   if (error) return <Text>Błąd: {error}</Text>;
 
   return (
-    <View className="p-5">
-      <Text className="text-2xl font-bold mb-4">Kolekcja</Text>
-      <Button
-        onPress={() => {
-          fetchGnomes();
-        }}
-      >
-        <Text className="text-white">Pobierz Krasnale</Text>
-      </Button>
-      <FlatList
-        data={gnomes}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Text style={{ fontSize: 18, marginTop: 5, color: "black" }}>
-            {item.name}
-          </Text>
-        )}
-      />
+    <View className="p-5 bg-background min-h-screen">
+      <Text className="text-2xl font-bold mb-4 color-white">Kolekcja</Text>
+      <View className="flex flex-row gap-4 mt-4 justify-center">
+        {gnomes.map((item) => (
+          <View key={item.id} className="w-1/3 p-1">
+            <GnomeCard
+              image={require("@/assets/images/placeholder.png")}
+              text={item.name}
+            />
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
