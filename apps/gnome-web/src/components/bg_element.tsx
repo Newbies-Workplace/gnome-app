b04e12fff5d6fc6afb4cff3d98c099e580f8f09e
N3s4a <nesakrad56@gmail.com>
2025-03-26 17:25:59 +0100

    feat: google login

--- apps/gnome-web/src/components/bg_element.tsx
+++ apps/gnome-web/src/components/bg_element.tsx
@@ -9,7 +9,7 @@ export const BgElement: React.FC<BgElementProps> = ({
 }) => {
   return (
     <div
-      className="ml-5 bg-[#333] w-49/50 h-45/50 absolute top-20 transform -z-1"
+      className="ml-5 bg-[#333] w-49/50 h-45/50 absolute top-22 transform -z-1"
       style={{
         borderBottomRightRadius: roundedBr,
         borderBottomLeftRadius: roundedBl,
--- apps/gnome-web/src/pages/HomePage.tsx
+++ apps/gnome-web/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <div className="ml-5 bg-[#333] w-[98%] h-[1800px] rounded-br-[350px] rounded-bl-[350px] absolute top-20 z-0">
+      <div className="ml-5 bg-[#333] w-[98%] h-[1800px] rounded-br-[350px] rounded-bl-[350px] absolute top-22 z-0">
         <div className="bg-[#272927] w-[100%] h-[350px] rounded-r-[150px] absolute z-1 flex flex-col justify-center items-start px-50 text-center">
           <div className="flex flex-row text-lg text-gray-300 text-[20px] ">
             <div>
