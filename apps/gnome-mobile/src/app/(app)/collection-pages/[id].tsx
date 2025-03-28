3fdbaf130b078b7b35e9adf7e563edf2d0d0503b
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-03-28 15:27:44 +0100

    feat: go back to collection

--- apps/gnome-mobile/src/app/(app)/collection-pages/[id].tsx
+++ apps/gnome-mobile/src/app/(app)/collection-pages/[id].tsx
@@ -1,16 +1,35 @@
+import ArrowLeft from "@/assets/icons/arrow-left.svg";
 import DateIcon from "@/assets/icons/date.svg";
 import FoundIcon from "@/assets/icons/found.svg";
 import { GnomeCard } from "@/components/ui/GnomeCard";
 import { useGnomeStore } from "@/store/useGnomeStore";
+import { useNavigation } from "@react-navigation/native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 const GnomeDetail = () => {
   const { id } = useLocalSearchParams(); // Get gnome ID from URL
   const { gnomes } = useGnomeStore(); // Fetch gnome list from store
   const [gnome, setGnome] = useState(null);
   const router = useRouter();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity className="p-5" onPress={() => router.back()}>
+          <ArrowLeft className="w-7 h-7" />
+        </TouchableOpacity>
+      ),
+      headerTitleAlign: "center",
+      headerStyle: {
+        backgroundColor: "#131413",
+      },
+      headerShadowVisible: false,
+      headerShown: true,
+    });
+  }, [navigation]);
 
   useEffect(() => {
     if (id) {
@@ -62,17 +81,17 @@ const GnomeDetail = () => {
       <View className="flex-row justify-center mt-5">
         <GnomeCard
           image={require("@/assets/images/placeholder.png")}
-          text=""
+          text="?"
           onClick={() => router.push("/collection")}
         />
         <GnomeCard
           image={require("@/assets/images/placeholder.png")}
-          text=""
+          text="?"
           onClick={() => router.push("/collection")}
         />
         <GnomeCard
           image={require("@/assets/images/placeholder.png")}
-          text=""
+          text="?"
           onClick={() => router.push("/collection")}
         />
       </View>
