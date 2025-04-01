daa28e2d60eb4c7286f5115bd9f362e0da09258f
N3s4a <nesakrad56@gmail.com>
2025-04-01 13:04:54 +0200

    fix: gnomePage

--- apps/gnome-web/src/pages/GnomePage.tsx
+++ apps/gnome-web/src/pages/GnomePage.tsx
@@ -1,13 +1,10 @@
 import Navbar from "../components/Navbar";
 import { BgElement } from "../components/bg_element";
-import InteractiveMapPicker from "../components/mapPicker/InternalMapPicker";
-
 export const GnomePage = () => {
   return (
     <div>
       <Navbar />
       <BgElement className="rounded-[15px]" />
-      <InteractiveMapPicker />
     </div>
   );
 };
