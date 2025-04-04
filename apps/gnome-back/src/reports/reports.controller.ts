a4e7c6d9d8163080945bd5839971c996abd5e27c
mati <MeenQ12@gmail.com>
2025-04-04 10:17:19 +0200

    Revert "image do reportu"
    This reverts commit cff0d550ce1ad9ff308541eb74ce66d944af46b0.

--- apps/gnome-back/src/reports/reports.controller.ts
+++ apps/gnome-back/src/reports/reports.controller.ts
@@ -1,6 +1,4 @@
-import { JWTUser } from "@/auth/jwt/JWTUser";
 import { JwtGuard } from "@/auth/jwt/jwt.guard";
-import { MinioService } from "@/minio/minio.service";
 import { Role } from "@/role/role.decorator";
 import { RoleGuard } from "@/roleguard/role.guard";
 import {
@@ -8,90 +6,67 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
-  ParseFilePipe,
   Post,
-  UploadedFile,
   UseGuards,
-  UseInterceptors,
 } from "@nestjs/common";
-import { FileTypeValidator, MaxFileSizeValidator } from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
 import { CreateReportRequest } from "@repo/shared/requests";
+import { ReportResponse } from "@repo/shared/responses";
 import { ReportsService } from "./reports.service";
+
 @Controller("reports")
 export class ReportsController {
-  constructor(
-    private readonly minioService: MinioService,
-    private readonly reportsService: ReportsService,
-  ) {}
+  constructor(private readonly reportsService: ReportsService) {}
+
   @Get("")
   @UseGuards(JwtGuard, RoleGuard)
   @Role(["ADMIN"])
-  getAllReports() {
-    /* zwraca wszystkie reporty i ich ilosc */
-    return this.reportsService.getAllReports();
+  async getAllReports() {
+    const allReports = await this.reportsService.getAllReports();
+
+    return allReports;
   }
+
   @Post("")
-  @UseInterceptors(FileInterceptor("file"))
   async createReport(
     @Body() body: CreateReportRequest,
-    @UploadedFile(
-      new ParseFilePipe({
-        fileIsRequired: false,
-        validators: [
-          new MaxFileSizeValidator({ maxSize: 10_000_000 }), // 10MB
-          new FileTypeValidator({ fileType: "image/jpeg" }),
-        ],
-      }),
-    )
-    file: Express.Multer.File,
-  ) {
-    await this.minioService.createBucketIfNotExists();
-
-    let fileUrl = null;
-    if (file) {
-      const fileName = `xd.jpg`;
-      const catalogueName = `reportImages`;
-      const filePath = `${catalogueName}/${fileName}`;
-
-      await this.minioService.uploadFile(file, fileName, catalogueName);
-      fileUrl = await this.minioService.getFileUrl(filePath);
-    }
-    const latitude = body.latitude;
-    const longitude = body.longitude;
-    return this.reportsService.createReport(
+  ): Promise<ReportResponse> {
+    const createReport = await this.reportsService.createReport(
       body.gnomeName,
-      fileUrl,
-      latitude,
-      longitude,
+      body.pictureUrl,
+      body.latitude,
+      body.longitude,
       body.location,
       body.reportAuthor,
     );
+    return createReport;
   }
 
   @Get(":id")
   @UseGuards(JwtGuard, RoleGuard)
   @Role(["ADMIN"])
-  getUniqueReport(@Param("id") id: string) {
-    return this.reportsService.getUniqueReport(id);
+  async getUniqueReport(@Param("id") id: string): Promise<ReportResponse> {
+    const uniqueReport = await this.reportsService.getUniqueReport(id);
+
+    return uniqueReport;
   }
 
   @Delete(":id")
   @UseGuards(JwtGuard, RoleGuard)
   @Role(["ADMIN"])
-  deleteReport(@Param("id") id: string) {
-    return this.reportsService.deleteReport(id);
+  async deleteReport(@Param("id") id: string): Promise<ReportResponse> {
+    await this.reportsService.getUniqueReport(id);
+    const deletedReport = await this.reportsService.deleteReport(id);
+    return deletedReport;
   }
 
   @Delete("")
   @UseGuards(JwtGuard, RoleGuard)
   @Role(["ADMIN"])
-  deleteAllReports() {
+  async deleteAllReports(): Promise<number> {
     /* zwraca liczbe usunietych zgloszen */
-    return this.reportsService.deleteAllReports();
+    await this.reportsService.getAllReports();
+    return await this.reportsService.deleteAllReports();
   }
-}
-function getUniqueReport(arg0: any, id: any, string: any) {
-  throw new Error("Function not implemented.");
 }
