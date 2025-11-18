5ca64462cedc8799079124e3af4254616f354039
Igor P <famokytl.p@gmail.com>
2025-11-18 12:02:40 +0100

    web

--- apps/gnome-web/src/main.tsx
+++ apps/gnome-web/src/main.tsx
@@ -1,34 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AddGnomePage } from "./pages/AddGnomePage.tsx";
-import { AdminPage } from "./pages/AdminPage.tsx";
-import { GnomePage } from "./pages/GnomePage.tsx";
-import { HomePage } from "./pages/HomePage.tsx";
-import InfoAddPage from "./pages/InfoAddPage";
-import { LogAdminPage } from "./pages/LogAdminPage.tsx";
-import { LostInfoPage } from "./pages/LostInfoPage.tsx";
-import { ReportPage } from "./pages/ReportPage.tsx";
+import App from "./App.tsx";
 
-const clientId = import.meta.env.VITE_PUBLIC_GOOGLE_WEB_CLIENT_ID;
-
-// biome-ignore lint/style/noNonNullAssertion: always present
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId={clientId}>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="report" element={<ReportPage />} />
-        <Route path="gnomes/:gnomeId" element={<GnomePage />} />
-        <Route path="admin">
-          <Route index element={<AdminPage />} />
-          <Route path="infoadd" element={<InfoAddPage />} />
-          <Route path="lostinfo" element={<LostInfoPage />} />
-          <Route path="addgnome" element={<AddGnomePage />} />
-          <Route path="logadmin" element={<LogAdminPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </GoogleOAuthProvider>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
