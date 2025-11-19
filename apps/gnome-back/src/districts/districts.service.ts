604eb32561613f493a8c3c6f2ea80c88a263e0c7
Mateusz <meenq12@gmail.com>
2025-11-19 10:33:19 +0100

    dobra bbox obliczanie zrobione

--- apps/gnome-back/src/districts/districts.service.ts
+++ apps/gnome-back/src/districts/districts.service.ts
@@ -17,25 +17,25 @@ export class DistrictsService {
   }
 
   async createDistricts(name: string, points: any = null) {
-    const coordinates = JSON.stringify(points);
-    const coords = JSON.parse(coordinates);
+    const alabama = JSON.stringify(points);
+    const coords = JSON.parse(alabama);
 
-    // const xs = coords.map(p => p[0])
-    // const ys = coords.map(p => p[1])
-    console.log(coords);
-    // const bbox = {
-    //     minX: Math.min(...xs),
-    //     maxX: Math.max(...xs),
-    //     minY: Math.min(...ys),
-    //     maxY: Math.max(...ys),
-    // }
+    const xs = coords.coordinates.map((p) => p[0]);
+    const ys = coords.coordinates.map((p) => p[1]);
+    console.log(xs);
+    const bbox = {
+      minX: Math.min(...xs),
+      maxX: Math.max(...xs),
+      minY: Math.min(...ys),
+      maxY: Math.max(...ys),
+    };
 
     return this.prismaService.districts.create({
       data: {
-        id: 9,
+        id: 11,
         name: name,
         points: points,
-        bbox: points,
+        bbox: bbox,
       },
     });
   }
