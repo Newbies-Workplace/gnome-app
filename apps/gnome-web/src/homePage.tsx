e0d982fdff0d6689ee4c33bc85244244b2e08a3f
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-19 18:19:23 +0100

    homePage added

---
+++ apps/gnome-web/src/homePage.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen bg-[url('assets/images/background.png')] bg-repeat">
+        <h1 className="text-red-400">Krasnale</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
