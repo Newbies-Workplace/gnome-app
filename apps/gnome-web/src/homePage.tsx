8095cd9eb062b4e2b916832d9bc737df920c8401
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-20 14:35:50 +0100

    button color changed

--- apps/gnome-web/src/homePage.tsx
+++ apps/gnome-web/src/homePage.tsx
@@ -17,7 +17,7 @@ function HomePage() {
         <div className="flex items-center justify-between">
           <h1 className="text-white text-5xl font-Afacad">Krasnale</h1>
           {/* Zaloguj button with icon */}
-          <button className="bg-red-700 text-white text-2xl font-Afacad px-6 py-3 rounded-4xl hover:opacity-90 transition flex items-center gap-2">
+          <button className="bg-[#D6484A] text-white text-2xl font-Afacad px-6 py-3 rounded-4xl hover:opacity-90 transition flex items-center gap-2">
             <img src={loginIcon} alt="login icon" className="w-6 h-6" />
             <Link to="/login" className="text-white">
               Zaloguj się
@@ -158,7 +158,7 @@ function HomePage() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="bg-red-700 text-white text-2xl w-100 px-6 py-3 rounded-4xl hover:opacity-90 transition mt-2 mb-2 font-Afacad">
+            <button className="bg-[#D6484A] text-white text-2xl w-100 px-6 py-3 rounded-4xl hover:opacity-90 transition mt-2 mb-2 font-Afacad">
               Pobierz na Android
             </button>
           </a>
@@ -168,7 +168,7 @@ function HomePage() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button className="bg-red-700 text-white text-2xl w-100 px-6 py-3 rounded-4xl hover:opacity-90 transition mt-2 mb-20 font-Afacad">
+            <button className="bg-[#D6484A] text-white text-2xl w-100 px-6 py-3 rounded-4xl hover:opacity-90 transition mt-2 mb-20 font-Afacad">
               Pobierz na iOS
             </button>
           </a>
