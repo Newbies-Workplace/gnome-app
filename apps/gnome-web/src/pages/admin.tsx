8bf945b3e6214708a192cb1f471690951fc6e1ca
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-24 14:48:28 +0100

    fully working map

--- apps/gnome-web/src/pages/admin.tsx
+++ apps/gnome-web/src/pages/admin.tsx
@@ -1,8 +1,17 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import backgroundImage from "@/assets/images/background.png";
 import { MapStyle } from "@/components/map-styles";
 
 export default function AdminPage() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (loadError) {
+    console.error("Błąd ładowania Google Maps:", loadError);
+  }
+
   const mapOptions = {
     fullscreenControl: false,
     mapTypeControl: true,
@@ -13,23 +22,32 @@ export default function AdminPage() {
 
   return (
     <div
-      className="fixed inset-0 bg-cover bg-center bg-repeat"
+      className="h-screen w-screen bg-cover bg-center bg-no-repeat flex flex-col"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="flex">
-        <div className="p-4 w-3/4 h-screen">
-          <LoadScript
-            googleMapsApiKey={import.meta.env.VITE_PUBLIC_GOOGLE_MAPS_API_KEY}
-          >
+      <div className="w-full p-4 flex justify-between items-center bg-transparent">
+        <div className="flex gap-4">
+          <button className="bg-primary-gray text-white text-xl font-Afacad px-6 py-2 rounded-3xl hover:opacity-90 transition">
+            Główna
+          </button>
+          <button className="bg-primary-color text-white text-xl font-Afacad px-6 py-2 rounded-3xl hover:opacity-90 transition">
+            Wyloguj
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-grow">
+        <div className="w-3/4 h-full p-4">
+          {isLoaded && (
             <GoogleMap
               mapContainerStyle={{ width: "100%", height: "100%" }}
               center={{ lat: 51.105, lng: 17.038 }}
               zoom={12}
               options={mapOptions}
             />
-          </LoadScript>
+          )}
         </div>
-        <div className="p-4 w-1/4 h-screen">{/* admin panel */}</div>
+        <div className="w-1/4 h-full p-4">{/* admin panel */}</div>
       </div>
     </div>
   );
