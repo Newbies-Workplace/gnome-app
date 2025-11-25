1f9dbe8cba041f5742b7550f0f812614628dcda5
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-25 23:38:12 +0100

    half working panel

--- apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
+++ apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
@@ -1,5 +1,12 @@
 function GnomePanel() {
-  return <div className="text-center text-white">Gnomes Panel</div>;
+  return (
+    <div className="overflow-y-auto flex flex-col h-full m-2">
+      <div className="text-center text-white text-3xl font-bold font-Afacad">
+        Panel Krasnali
+      </div>
+      <button>Dodaj nowego krasnala</button>
+    </div>
+  );
 }
 
 export default GnomePanel;
