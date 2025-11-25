373b8de3f1e0548df278aaea60472839c0b131bd
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-25 23:52:58 +0100

    demo gnomes-panel

--- apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
+++ apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
@@ -1,10 +1,16 @@
+import GnomesList from "@/components/ui/admin/gnomes-list";
+
 function GnomePanel() {
   return (
     <div className="overflow-y-auto flex flex-col h-full m-2">
       <div className="text-center text-white text-3xl font-bold font-Afacad">
         Panel Krasnali
       </div>
-      <button>Dodaj nowego krasnala</button>
+      <button className="text-center text-white">
+        {" "}
+        + Dodaj nowego krasnala
+      </button>
+      <GnomesList />
     </div>
   );
 }
