97d7aceb4e1a768d7ab88b3c49c199b7555cbee3
kqshee <kamilawydraa@gmail.com>
2025-11-26 14:21:15 +0100

    working login require

--- apps/gnome-web/src/App.tsx
+++ apps/gnome-web/src/App.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import AdminPage from "@/pages/admin";
 import HomePage from "@/pages/home";
 import LoginPage from "@/pages/login";
 import LoginCallback from "@/pages/login-callback";
+import { RequireAuth } from "@/pages/requireAuth";
+import { useAuthStore } from "./store/useAuthStore";
 
 function App() {
+  const init = useAuthStore((s) => s.init);
+
+  useEffect(() => {
+    void init();
+  }, []);
   return (
     <Routes>
       <Route index element={<HomePage />} />
@@ -14,7 +22,14 @@ function App() {
         <Route path="callback" element={<LoginCallback />} />
       </Route>
 
-      <Route path="admin" element={<AdminPage />} />
+      <Route
+        path="admin"
+        element={
+          <RequireAuth>
+            <AdminPage />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
