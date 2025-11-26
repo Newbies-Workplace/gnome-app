c182c7f76e64bc61dff0a30e11ab4d4faf98c7e6
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2025-11-26 13:11:18 +0100

    improved gnome panel

--- apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
+++ apps/gnome-web/src/pages/gnomes/gnomes-panel.tsx
@@ -1,15 +1,24 @@
+import AddIcon from "@/assets/icons/add-icon.svg";
 import GnomesList from "@/components/ui/admin/gnomes-list";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function GnomesPanel() {
   return (
-    <div className="overflow-y-auto flex flex-col h-full m-2">
+    <div className="overflow-y-auto flex flex-col h-full gap-2">
       <div className="text-center text-white text-3xl font-bold font-Afacad">
         Panel Krasnali
       </div>
-      <button className="text-center text-white">
-        {" "}
-        + Dodaj nowego krasnala
-      </button>
+      <Input
+        id="search-gnome"
+        type="text"
+        placeholder="Wyszukaj krasnala..."
+        className="text-white font-Afacad rounded-4xl bg-[#282B28] border-none outline-none"
+      />
+      <Button className="text-center text-white font-Afacad bg-primary-color border-none rounded-4xl">
+        <img src={AddIcon} alt="add" />
+        Dodaj nowego krasnala
+      </Button>
       <GnomesList />
     </div>
   );
