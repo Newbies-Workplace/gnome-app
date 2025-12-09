924328ddab577a4e1c2d7a7638838097fc79aaf5
Igor P <famokytl.p@gmail.com>
2025-12-09 12:11:31 +0100

    layout splash screen

--- apps/gnome-mobile/src/app/_layout.tsx
+++ apps/gnome-mobile/src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import "../global.css";
 import BottomSheet, {
   BottomSheetModal,
@@ -7,13 +8,42 @@ import BottomSheet, {
 import { Portal, PortalHost } from "@rn-primitives/portal";
 import { StatusBar } from "expo-status-bar";
 import { cssInterop } from "nativewind";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Svg, { Circle, Rect, Text as SvgText } from "react-native-svg";
 import { useAuthStore } from "@/store/useAuthStore";
 
+SplashScreen.setOptions({
+  duration: 800,
+  fade: true,
+});
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [isReady, setisReady] = useState(false);
   const { init } = useAuthStore();
+
+  useEffect(() => {
+    async function initializeApp() {
+      try {
+        await init();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setisReady(true);
+      }
+    }
+    initializeApp();
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+  if (!isReady) {
+    return null;
+  }
 
   const sheetInteropConfig = {
     backgroundClassName: { target: "backgroundStyle" },
@@ -63,10 +93,6 @@ export default function RootLayout() {
       },
     },
   });
-
-  useEffect(() => {
-    init();
-  }, []);
 
   return (
     <GestureHandlerRootView>
