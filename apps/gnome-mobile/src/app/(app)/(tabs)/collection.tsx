54bcf336ea0fdc2e3ce2ec4774b85b1187051aee
Raficknew <rafi20302@gmail.com>
2025-12-28 00:23:24 +0100

    added sectionList on collection

--- apps/gnome-mobile/src/app/(app)/(tabs)/collection.tsx
+++ apps/gnome-mobile/src/app/(app)/(tabs)/collection.tsx
@@ -1,7 +1,14 @@
+import { GnomeResponse } from "@repo/shared/responses";
 import { useNavigation, useRouter } from "expo-router";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { FlatList, Text, View } from "react-native";
+import {
+  FlatList,
+  SectionList,
+  SectionListRenderItem,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LoadingScreen from "@/components/LoadingScreen";
 import { GnomeCard } from "@/components/ui/GnomeCard";
@@ -10,10 +17,59 @@ import { useGnomeStore } from "@/store/useGnomeStore";
 
 const Collection = () => {
   const { t } = useTranslation();
-  const { gnomes, error } = useGnomeStore();
+  const { gnomes, interactions, error } = useGnomeStore();
   const { getImageForGnome } = useGnomeImageStore();
   const router = useRouter();
   const navigation = useNavigation();
+  const collectedGnomeIds = interactions.map((i) => i.gnomeId);
+  const collectedGnomes = gnomes.filter((g) =>
+    collectedGnomeIds.includes(g.id),
+  );
+  const uncollectedGnomes = gnomes.filter(
+    (g) => !collectedGnomeIds.includes(g.id),
+  );
+  const gnomeSectionsData = [
+    { title: t("collection.collectedGnomes"), data: collectedGnomes },
+    { title: t("collection.uncollectedGnomes"), data: uncollectedGnomes },
+  ];
+
+  const renderGnome = (gnome: { id: string; name: string }) => {
+    const img = getImageForGnome(gnome.id);
+    return (
+      <View className="w-1/3" key={gnome.id}>
+        <GnomeCard
+          gnomeId={gnome.id}
+          text={gnome.name}
+          onClick={() => router.push(`/gnomes/${gnome.id}`)}
+          interaction={{
+            found: !!img,
+            userPicture: img?.assetUri,
+          }}
+        />
+      </View>
+    );
+  };
+
+  const renderItemList: SectionListRenderItem<GnomeResponse> = ({
+    section,
+    index,
+  }) => {
+    if (index % 3 !== 0) {
+      return null;
+    }
+
+    const item = section.data[index];
+    const middleItem = section.data[index + 1];
+    const lastItem = section.data[index + 2];
+
+    return (
+      <View className="flex-row">
+        {renderGnome(item)}
+        {middleItem && renderGnome(middleItem)}
+        {lastItem && renderGnome(lastItem)}
+      </View>
+    );
+  };
 
   useEffect(() => {
     navigation.setOptions({
@@ -35,29 +91,13 @@ const Collection = () => {
 
   return (
     <SafeAreaView className={"flex-1 bg-primary-foreground"}>
-      <SectionTitle title={t("collection.collectedGnomes")} />
-      <FlatList
-        data={gnomes}
-        contentContainerClassName={"bg-primary-foreground"}
-        columnWrapperStyle={{ justifyContent: "flex-start" }}
-        keyExtractor={(item) => item.id?.toString()}
-        numColumns={3}
-        renderItem={({ item }) => {
-          const img = getImageForGnome(item.id);
-          return (
-            <View className="w-1/3" key={item.id}>
-              <GnomeCard
-                gnomeId={item.id}
-                text={item.name}
-                onClick={() => router.push(`/gnomes/${item.id}`)}
-                interaction={{
-                  found: !!img,
-                  userPicture: img?.assetUri,
-                }}
-              />
-            </View>
-          );
-        }}
+      <SectionList
+        sections={gnomeSectionsData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItemList}
+        renderSectionHeader={({ section: { title } }) => (
+          <SectionTitle>{title}</SectionTitle>
+        )}
         ListEmptyComponent={() =>
           error ? (
             <Text className="text-tekst">{t("common.genericError")}</Text>
@@ -66,13 +106,12 @@ const Collection = () => {
           )
         }
       />
-      <SectionTitle title={t("collection.uncollectedGnomes")} />
     </SafeAreaView>
   );
 };
 
-function SectionTitle({ title }: { title: string }) {
-  return <Text className="text-tekst font-bold text-lg">{title}</Text>;
+function SectionTitle({ children }: { children: React.ReactNode }) {
+  return <Text className="text-tekst font-bold text-lg">{children}</Text>;
 }
 
 export default Collection;
